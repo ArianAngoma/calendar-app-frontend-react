df3f311c39bb6911a663539d4de34d1b159315a6
unknown <arian.angoma@tecsup.edu.pe>
2021-09-29 19:21:29 -0500

    Configuración del calendario

--- src/components/calendar/CalendarScreen.js
+++ src/components/calendar/CalendarScreen.js
@@ -1,11 +1,16 @@
 import {Calendar, momentLocalizer} from 'react-big-calendar'
 import moment from 'moment';
+import 'moment/locale/es';
 
 /* Importaciones propias */
 import {Navbar} from "../ui/Navbar";
+import {messages} from "../../helpers/calendar-messages-es";
 
 /* Importacion del estilo de BigCalendar */
 import 'react-big-calendar/lib/css/react-big-calendar.css';
+
+/* Configuración del idioma de moment */
+moment.locale('es');
 
 /* Configuración de react big calendar */
 const localizer = momentLocalizer(moment);
@@ -19,6 +24,19 @@ const events = [{
 }]
 
 export const CalendarScreen = () => {
+    const eventStyleGetter = (event, start, end, isSelected) => {
+        // console.log(event, start, end, isSelected);
+        return {
+            style: {
+                backgroundColor: '#367CF7',
+                borderRadius: '0px',
+                opacity: 0.8,
+                display: 'block',
+                color: 'white'
+            }
+        }
+    }
+
     return (
         <div className="calendar-screen">
             <Navbar/>
@@ -27,7 +45,9 @@ export const CalendarScreen = () => {
                 localizer={localizer}
                 events={events}
                 startAccessor="start"
-                endAccessor="end"/>
+                endAccessor="end"
+                messages={messages}
+                eventPropGetter={eventStyleGetter}/>
         </div>
     )
 }
