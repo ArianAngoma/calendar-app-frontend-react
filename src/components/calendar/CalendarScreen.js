ee5cb8970a2a8faf363906a1dcb355aabd4a03b1
unknown <arian.angoma@tecsup.edu.pe>
2021-09-29 19:05:19 -0500

    Calendario React Big Calendar

--- src/components/calendar/CalendarScreen.js
+++ src/components/calendar/CalendarScreen.js
@@ -1,10 +1,33 @@
+import {Calendar, momentLocalizer} from 'react-big-calendar'
+import moment from 'moment';
+
 /* Importaciones propias */
 import {Navbar} from "../ui/Navbar";
 
+/* Importacion del estilo de BigCalendar */
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+
+/* Configuración de react big calendar */
+const localizer = momentLocalizer(moment);
+
+/* Lista de eventos */
+const events = [{
+    title: 'Cumpleaños de Arian',
+    start: moment().toDate(), // new Date()
+    end: moment().add(2, 'hours').toDate(),
+    bgColor: '#fafafa'
+}]
+
 export const CalendarScreen = () => {
     return (
-        <div>
+        <div className="calendar-screen">
             <Navbar/>
+
+            <Calendar
+                localizer={localizer}
+                events={events}
+                startAccessor="start"
+                endAccessor="end"/>
         </div>
     )
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+/* Estilos globales */
+import './styles.css';
+
 /* Importaciones propias */
 import {CalendarApp} from "./CalendarApp";
 
