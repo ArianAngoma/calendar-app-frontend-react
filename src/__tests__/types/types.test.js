cdc67626e72283879a6bea009bcd919f775b5e2c
unknown <arian.angoma@tescup.edu.pe>
2021-10-06 23:11:10 -0500

    Configuración para las pruebas y primera prueba en Types de las acciones

---
+++ src/__tests__/types/types.test.js
@@ -0,0 +1,25 @@
+/* Importaciones propias */
+import {types} from '../../types/types';
+
+describe('Pruebas en Types', () => {
+    test('Debería de ser iguales los types', () => {
+        expect(types).toEqual({
+            uiOpenModal: '[ui] Open Modal',
+            uiOpenModalWithSlotCalendar: '[ui] Open Modal With Slot Calendar',
+            uiCloseModal: '[ui] Close Modal',
+
+            eventSetActive: '[event] Set Active',
+            eventLogout: '[event] Clear Events Logout',
+            eventClearActiveEvent: '[event] Clear Active Event',
+            eventStartAddNew: '[event] Start Add New',
+            eventAddNew: '[event] Add New',
+            eventUpdated: '[event] Event Updated',
+            eventDeleted: '[event] Event Deleted',
+            eventLoaded: '[event] Events Loaded',
+
+            authCheckingFinish: '[auth] Finish Checking Login State',
+            authLogin: '[auth] Login',
+            authLogout: '[auth] Logout'
+        });
+    });
+});
