0a6e0c1509f3d59b5980f09ade333fe37127dfdb
unknown <arian.angoma@tescup.edu.pe>
2021-10-07 04:00:23 -0500

    Prueba en el helper fetchWithToken

--- src/__tests__/helpers/fetch.test.js
+++ src/__tests__/helpers/fetch.test.js
@@ -1,12 +1,25 @@
 /* Importaciones propias */
-import {fetchNoToken} from '../../helpers/fetch';
+import {fetchNoToken, fetchWithToken} from '../../helpers/fetch';
 
 describe('Pruebas en el Helper Fetch', () => {
+    let token;
+
     test('Debería de funcionar fetchNoToken', async () => {
         const resp = await fetchNoToken('auth', {email: 'test1@gmail.com', password: '123456'}, 'POST');
         expect(resp instanceof Response).toBe(true);
 
-        const body = await resp.json();
-        expect(body.ok).toBe(true);
+        const data = await resp.json();
+        expect(data.ok).toBe(true);
+
+        /* Guardar token */
+        token = data.token;
+    });
+
+    test('Debería de funcionar fetchWithToken', async () => {
+        localStorage.setItem('token', token);
+        const resp = await fetchWithToken('events/615ae9d4082e922dd967762f', {}, 'DELETE');
+        const data = await resp.json();
+        // console.log(data);
+        expect(data.ok).toBe(false);
     });
 });
