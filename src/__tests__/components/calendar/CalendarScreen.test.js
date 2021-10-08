948cb45df17e60864dfcdfe9c412f869e7bce6ee
unknown <arian.angoma@tecsup.edu.pe>
2021-10-08 05:21:52 -0500

    Pruebas en el componente CalendarScreen

--- src/__tests__/components/calendar/CalendarScreen.test.js
+++ src/__tests__/components/calendar/CalendarScreen.test.js
@@ -2,14 +2,22 @@ import {mount} from 'enzyme';
 import {Provider} from 'react-redux';
 import configureStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
+import {act} from '@testing-library/react';
 
 /* Importaciones propias */
 import {CalendarScreen} from '../../../components/calendar/CalendarScreen';
+import {messages} from '../../../helpers/calendar-messages-es';
+import {types} from '../../../types/types';
+import {eventSetActive} from '../../../actions/events';
 
-/* Mock para la acción eventStartDelete */
-/*jest.mock('../../../actions/events', () => ({
-    eventStartDelete: jest.fn()
-}));*/
+/* Mock para la acción eventSetActive */
+jest.mock('../../../actions/events', () => ({
+    eventSetActive: jest.fn(),
+    eventStartLoading: jest.fn()
+}));
+
+/* Mock para el localStorage */
+Storage.prototype.setItem = jest.fn();
 
 /* Configuración del Store */
 const middlewares = [thunk];
@@ -43,5 +51,24 @@ const wrapper = mount(
 describe('Pruebas en el componente <CalendarScreen/>', () => {
     test('Debería de mostrarse correctamente', () => {
         expect(wrapper).toMatchSnapshot();
+    });
+
+    test('Debería de ejecutar correctamente las interacciones del calendario', () => {
+        const calendar = wrapper.find('Calendar');
+
+        const calendarMessages = calendar.prop('messages');
+        // console.log(calendarMessages);
+        expect(calendarMessages).toEqual(messages);
+
+        calendar.prop('onDoubleClickEvent')();
+        expect(store.dispatch).toHaveBeenLastCalledWith({type: types.uiOpenModal});
+
+        calendar.prop('onSelectEvent')({start: 'Hello World'});
+        expect(eventSetActive).toHaveBeenLastCalledWith({start: 'Hello World'});
+
+        act(() => {
+            calendar.prop('onView')('week');
+            expect(localStorage.setItem).toHaveBeenLastCalledWith("last-view", "week");
+        });
     });
 });
