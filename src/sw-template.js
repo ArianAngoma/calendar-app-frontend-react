afb0ac4436e1c88048abd86273953571b3bf881a
ArianAngoma <arian.angoma@tecsup.edu.pe>
2021-12-11 21:24:59 -0500

    Workbox Cache Strategies

--- src/sw-template.js
+++ src/sw-template.js
@@ -1,3 +1,16 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.2.0/workbox-sw.js');
 
 workbox.precaching.precacheAndRoute(self.__WB_MANIFEST);
+
+const {registerRoute} = workbox.routing;
+const {CacheFirst} = workbox.strategies;
+
+registerRoute(
+    new RegExp('https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css'),
+    new CacheFirst()
+)
+
+registerRoute(
+    new RegExp('https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css'),
+    new CacheFirst()
+)
