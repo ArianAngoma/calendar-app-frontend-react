0bd73e68850fe74505252604a8c342991f727928
ArianAngoma <arian.angoma@tecsup.edu.pe>
2021-12-12 21:09:58 -0500

    Verificar token offline

--- src/sw-template.js
+++ src/sw-template.js
@@ -3,7 +3,17 @@ importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.2.0/workbox
 workbox.precaching.precacheAndRoute(self.__WB_MANIFEST);
 
 const {registerRoute} = workbox.routing;
-const {CacheFirst} = workbox.strategies;
+const {CacheFirst, NetworkFirst} = workbox.strategies;
+
+registerRoute(
+    new RegExp('https://calendar-app-arianjs.herokuapp.com/api/auth/renew-token'),
+    new NetworkFirst()
+)
+
+registerRoute(
+    new RegExp('https://calendar-app-arianjs.herokuapp.com/api/events'),
+    new NetworkFirst()
+)
 
 registerRoute(
     new RegExp('https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css'),
